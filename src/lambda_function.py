97a0df9cdd72388302dd7e78326ae250af9be6ee
eymea <eduardo@eymea.com>
2025-03-10 20:51:16 +1100

    v5
    Hide ARN

--- src/lambda_function.py
+++ src/lambda_function.py
@@ -8,7 +8,7 @@ def lambda_handler(event, context):
     
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! V4')
+        'body': json.dumps('Hello from Lambda! V5 Hide ARN')
     }
 
 if __name__ == "__main__":
