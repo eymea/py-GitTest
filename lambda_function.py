aaf0d38995930d90e3de949ee89c409ddb601945
eymea <eduardo@eymea.com>
2025-03-10 17:10:04 +1100

    Create lambda_function.py
    Initial

---
+++ lambda_function.py
@@ -0,0 +1,15 @@
+import json
+import sys
+
+def lambda_handler(event, context):
+    # TODO implement
+
+    print(sys.version)
+    
+    return {
+        'statusCode': 200,
+        'body': json.dumps('Hello from Lambda!')
+    }
+
+if __name__ == "__main__":
+    print(lambda_handler({}, {}))
